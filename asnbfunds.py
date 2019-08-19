ce215438d9dcafc0ecf2495790b95a76fa3213b5
Ng, Jerry Shi Heng <jerry.shi.heng.ng@intel.com>
2019-08-19 18:55:27 +0800

    Funds classes + their elements

---
+++ asnbfunds.py
@@ -0,0 +1,16 @@
+class AmanahSahamMalaysia:
+    name = 'Amanah Saham Malaysia (ASM)'
+    transaction_drop_down_xpath_name = '//*[@id="form1_ASM"]/div/button'
+    additional_investment_element_name = 'submit_buy_ASM'
+
+
+class AmanahSahamMalaysia2:
+    name = 'Amanah Saham Malaysia 2 - Wawasan (ASW)'
+    transaction_drop_down_xpath_name = '//*[@id="form1_ASW"]/div/button'
+    additional_investment_element_name = 'submit_buy_ASW'
+
+
+class AmanahSahamMalaysia3:
+    name = 'Amanah Saham Malaysia 3 (AS1M)'
+    transaction_drop_down_xpath_name = '//*[@id="form1_AS1M"]/div/button'
+    additional_investment_element_name = 'submit_buy_AS1M'
