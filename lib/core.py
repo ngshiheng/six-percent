fb97e0bca13e3efb849b5ff5f9fe29e57cb9bec5
Shi Heng Ng <ngshiheng@gmail.com>
2021-10-01 13:05:07 +0800

    Refactor xpaths into constants

--- lib/core.py
+++ lib/core.py
@@ -10,7 +10,16 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
-from lib.constants import MAX_PURCHASE_RETRY_ATTEMPTS, TIMEOUT_LIMIT, TOTAL_FUND_COUNT
+from lib.constants import (BANK_DROPDOWN_SELECTION_XPATH,
+                           ENGLISH_LANGUAGE_BUTTON_XPATH,
+                           ERROR_PROMPT_OK_BUTTON_XPATH, FUNDS_XPATH,
+                           INVESTMENT_AMOUNT_XPATH,
+                           MAX_PURCHASE_RETRY_ATTEMPTS, PAYMENT_TIMEOUT_LIMIT,
+                           PEP_DECLARATION_PROMPT_NEXT_BUTTON_XPATH,
+                           PEP_DECLARATION_PROMPT_XPATH, PORTFOLIO_URL_XPATH,
+                           PROMPT_OK_BUTTON_XPATH, SUBMIT_BUTTON_XPATH,
+                           TERMS_AND_CONDITIONS_CHECKBOX_XPATH, TIMEOUT_LIMIT,
+                           TOTAL_FUND_COUNT)
 
 logging.basicConfig(level=logging.INFO)
 
@@ -71,65 +80,60 @@ class SixPercent:
         """
         Purchase ASNB Fixed Price UT units
         """
-        browser.find_element_by_xpath("//a[@name='en']").click()  # Always set language to English
+        wait = WebDriverWait(browser, TIMEOUT_LIMIT)
+        browser.find_element_by_xpath(ENGLISH_LANGUAGE_BUTTON_XPATH).click()  # Always set language to English
 
         try:
-            FUNDS_XPATH = '//div[@class="bg-white mb-3 w-full mx-auto text-gray-500 grid grid-cols-4 md:grid-cols-5 xl:grid-cols-6 justify-between rounded-lg px-0 py-4 shadow-lg dark:bg-gray-700 dark:text-gray-400 lg:h-48"]'
 
-            wait = WebDriverWait(browser, 2)
             for i in range(TOTAL_FUND_COUNT):
                 # Select fund to purchase
-                WebDriverWait(browser, TIMEOUT_LIMIT).until(EC.presence_of_all_elements_located((By.XPATH, FUNDS_XPATH)))[i].click()
+                logging.info("Selecting fund to invest")
+                wait.until(EC.presence_of_all_elements_located((By.XPATH, FUNDS_XPATH)))[i].click()
 
                 # Handle cases where the funds are unavailable (i.e. due to distribution of dividends)
-                with suppress(TimeoutException):
-                    wait.until(EC.element_to_be_clickable((By.XPATH, "//p[contains(text(), 'not available')]")))
-                    browser.find_element_by_xpath("//button[contains(text(), 'OK')]").click()
-                    continue
+                with suppress(NoSuchElementException):
+                    browser.find_element_by_xpath(ERROR_PROMPT_OK_BUTTON_XPATH).click()
+                    logging.info("Funds unavailable for purchase")
 
                 # Enter investment amount
-                amount_field = wait.until(EC.element_to_be_clickable((By.XPATH, "//input[@name='amount']")))
-                amount_field.send_keys(investment_amount)
+                logging.info(f"Entering investment amount RM {investment_amount}")
+                wait.until(EC.element_to_be_clickable((By.XPATH, INVESTMENT_AMOUNT_XPATH))).send_keys(investment_amount)
 
                 # Select bank of choice
-                wait.until(EC.element_to_be_clickable((By.XPATH, "//select[@name='banks']/option[@value='Maybank2U']"))).click()  # TODO: Allow users to select bank of choice from UI
+                logging.info("Selecting Maybank2U as payment bank of choice")
+                wait.until(EC.element_to_be_clickable((By.XPATH, BANK_DROPDOWN_SELECTION_XPATH))).click()  # TODO: Allow users to select bank of choice from UI
 
                 # Check the terms and condition checkbox
-                browser.find_element_by_xpath("//input[@type='checkbox']").click()
+                logging.info("Agreeing to terms and conditions")
+                browser.find_element_by_xpath(TERMS_AND_CONDITIONS_CHECKBOX_XPATH).click()
 
-                submit_purchase_button = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[@type='submit']")))
+                submit_purchase_button = wait.until(EC.element_to_be_clickable((By.XPATH, SUBMIT_BUTTON_XPATH)))
 
                 for attempt in range(MAX_PURCHASE_RETRY_ATTEMPTS):
                     submit_purchase_button.click()
 
                     # PEP declaration
                     with suppress(NoSuchElementException):
-                        browser.find_element_by_xpath("//h3[contains(text(), 'Declaration of PEP')]")
+                        browser.find_element_by_xpath(PEP_DECLARATION_PROMPT_XPATH)
                         logging.info('PEP declaration')
-                        browser.find_elements_by_xpath("//button[contains(text(), 'Next')]")[1].click()
-
-                    # Stop trying when blocked
-                    with suppress(TimeoutException):
-                        wait.until(EC.element_to_be_clickable((By.XPATH, "//p[contains(text(), 'Blocked')]")))
-                        browser.find_element_by_xpath("//button[contains(text(), 'OK')]").click()
-                        break
+                        browser.find_elements_by_xpath(PEP_DECLARATION_PROMPT_NEXT_BUTTON_XPATH)[1].click()
 
                     try:
-                        wait.until(EC.element_to_be_clickable((By.XPATH, "//p[contains(text(), 'insufficient units')]")))
+                        ok_button = wait.until(EC.element_to_be_clickable((By.XPATH, PROMPT_OK_BUTTON_XPATH)))
                         logging.info(f"The transaction was declined due to insufficient units available - {attempt + 1}")
-                        browser.find_element_by_xpath("//button[contains(text(), 'OK')]").click()
+                        ok_button.click()
 
-                    except TimeoutException:
+                    except (TimeoutException, NoSuchElementException):
                         logging.info('Please proceed to make payment')
-                        time.sleep(300)
+                        time.sleep(PAYMENT_TIMEOUT_LIMIT)
                         return None
 
                 # Return to main portfolio page
-                browser.find_elements_by_xpath("//a[@href='/portfolio']")[1].click()
+                browser.find_elements_by_xpath(PORTFOLIO_URL_XPATH)[-1].click()
                 continue
 
-        except TimeoutException:
-            WebDriverWait(browser, TIMEOUT_LIMIT).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Close')]"))).click()
+        except (TimeoutException, NoSuchElementException):
+            wait.until(EC.element_to_be_clickable((By.XPATH, ERROR_PROMPT_OK_BUTTON_XPATH))).click()
             logging.exception('Unable to purchase fund now')
 
         finally:
